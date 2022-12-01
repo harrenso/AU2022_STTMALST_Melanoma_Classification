47091ff5a705f9514070d00a8e9c65d04f7d73a7
OskierP <pakooskar@gmail.com>
2022-12-01 13:39:25 +0100

    centered a div

--- ArosAiWebsite/src/app/app.component.ts
+++ ArosAiWebsite/src/app/app.component.ts
@@ -8,44 +8,25 @@ import {Observable} from "rxjs";
 })
 export class AppComponent {
   title = 'ArosAiWebsite';
-  selectedFiles?: FileList;
-  selectedFileNames: string[] = [];
 
-  progressInfos: any[] = [];
-  message: string[] = [];
-
-  previews: string[] = [];
-  imageInfos?: Observable<any>;
+  msg: any;
+  url: any;
 
   uploadFiles() {
 
   }
 
-  selectFiles(event: any): void {
-    this.message = [];
-    this.progressInfos = [];
-    this.selectedFileNames = [];
-    this.selectedFiles = event.target.files;
-
-    this.previews = [];
-    if (this.selectedFiles && this.selectedFiles[0]) {
-      const numberOfFiles = this.selectedFiles.length;
-      for (let i = 0; i < numberOfFiles; i++) {
-        const reader = new FileReader();
-
-        reader.onload = (e: any) => {
-          console.log(e.target.result);
-          this.previews.push(e.target.result);
-        };
-
-        reader.readAsDataURL(this.selectedFiles[i]);
-
-        this.selectedFileNames.push(this.selectedFiles[i].name);
-      }
-    }
-  }
 
   onFileSelect(event: any) {
     console.log(event);
+
+
+    var reader = new FileReader();
+    reader.readAsDataURL(event.target.files[0]);
+
+    reader.onload = (_event) => {
+      this.msg = "";
+      this.url = reader.result;
+    }
   }
 }
